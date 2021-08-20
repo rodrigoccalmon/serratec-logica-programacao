177499118c9148f566cc26d9cade12537b69ec1c
Rodrigo Calmon <89034960+rodrigoccalmon@users.noreply.github.com>
2021-08-20 09:45:19 -0300

    Delete chamada.js

--- chamada.js
+++
@@ -1,4 +0,0 @@
-console.log("=================SORTEIO================");
-var nomes = ['Carlos', 'Rafael', 'Marcella', 'Marcos', 'Luana', 'Angelita', 'Daniel', 'Viviane', 'Rodrigo de Paula', 'Diego', 'Luciana', 'Guilherme', 'Leandro', 'Luciana', 'Nathan', 'Higor', 'Pedro', 'Victor', 'Gustavo', 'Karina', 'Gilnei', 'Andre', 'Adrian', 'Samuel', 'Raquel', 'Beatriz', 'Rodrigo'];
-nomes.sort();
-console.log(nomes);

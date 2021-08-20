0ac88b11de7e5d9e3bbf69089314113ede3b49a4
Rodrigo Calmon <89034960+rodrigoccalmon@users.noreply.github.com>
2021-08-20 09:45:13 -0300

    Delete desafioFaseUm.js

--- desafioFaseUm.js
+++
@@ -1,5 +0,0 @@
-console.log("\n")
-console.log("===============DESAFIO==================")
-console.log(" Bom dia, \n Boa tarde, \n Boa noite. ");
-console.log("========================================");
-console.log("\n")
